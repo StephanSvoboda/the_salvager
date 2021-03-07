c026dce45df86768b606f148530f8d5f87ad2b51
stephan <stephan@svoboda.or.at>
2021-03-07 11:51:18 +0100

    Add Pathfinding for Robots, they chase now the player

--- src/robot_ai_system.rs
+++ src/robot_ai_system.rs
@@ -1,23 +1,37 @@
 use specs::prelude::*;
-use super::{Viewshed, Robot, Name};
-use rltk::{field_of_view, Point, console};
+use super::{Viewshed, Robot, Name, Map, Position};
+use rltk::{Point, console};
 
 pub struct RobotAI {}
 
 impl <'a> System<'a> for RobotAI {
+    #[allow(clippy::type_complexity)]
     type SystemData = (
+        WriteExpect<'a, Map>,
         ReadExpect<'a, Point>,
-        ReadStorage<'a, Viewshed>,
+        WriteStorage<'a, Viewshed>,
         ReadStorage<'a, Robot>,
         ReadStorage<'a, Name>,
+        WriteStorage<'a, Position>
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (player_position, viewsheds, robots, names) = data;
+        let (mut map, player_position, mut viewsheds, robots, names, mut positions) = data;
 
-        for (_viewshed, _robot, _name) in (&viewsheds, &robots, &names).join() {
+        for (_viewshed, _robot, _name,mut pos) in (&mut viewsheds, &robots, &names, &mut positions).join() {
             if _viewshed.visible_tiles.contains(&*player_position){
                 console::log(format!("{} sees Player at {},{}",_name.name, player_position.x, player_position.y));
+                let path = rltk::a_star_search(
+                    map.xy_idx(pos.x, pos.y) as i32,
+                    map.xy_idx(player_position.x, player_position.y) as i32,
+                    &mut *map
+                );
+                if path.success && path.steps.len()>1 {
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    console::log(format!("{} hunts player moving to {},{}", _name.name, pos.x, pos.y));
+                    _viewshed.dirty = true;
+                }
             }
         }
     }
