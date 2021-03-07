e78912015062e70e7b43c58f25d4d7b4335cecdf
stephan <stephan@svoboda.or.at>
2021-03-07 14:23:30 +0100

    Add target detection for player

--- src/player.rs
+++ src/player.rs
@@ -1,17 +1,29 @@
 use rltk::Point;
-use rltk::{VirtualKeyCode, Rltk};
+use rltk::{VirtualKeyCode, Rltk, console};
 use specs::prelude::*;
-use super::{Position, Player, Map, State, Viewshed, RunState};
+use super::{Position, Player, Map, State, Viewshed, RunState, CombatStats};
 use std::cmp::{min, max};
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
     let mut viewshed = ecs.write_storage::<Viewshed>();
+    let combat_stats = ecs.read_storage::<CombatStats>();
     let map = ecs.fetch::<Map>();
 
     for (_player, pos, _viewshed) in (&mut players, &mut positions, &mut viewshed).join() {
         let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+
+        for potential_target in map.tile_content[destination_idx].iter() {
+            let target = combat_stats.get(*potential_target);
+            match target {
+                None => {}
+                Some(target) => {
+                    console::log(format!("Player stabs target"));
+                    return;
+                }
+            }
+        }
         if !map.blocked_tiles[destination_idx] {
             pos.x = min(79 , max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
